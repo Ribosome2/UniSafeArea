ad5811c2b968514e943edf5051c1c779662689d9
Ribosome2 <583154233@qq.com>
2023-10-16 07:20:54 +0800

    屏幕适配

--- Assets/Scripts/UIAdapt.cs
+++ Assets/Scripts/UIAdapt.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIAdapt : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class UIAdapt : MonoBehaviour
     public float maxY=1;
     public float referenceWidth = 600;
     public float referenceHeight = 800;
+    public CanvasScaler canvasScaler;
     // Update is called once per frame
     void Update()
     {
@@ -24,6 +26,21 @@ public class UIAdapt : MonoBehaviour
         var screenHeight = UnityEngine.Device.Screen.height;
         var safeArea = UnityEngine.Device.Screen.safeArea;
 
+        float screenRatio = (float)screenWidth / screenHeight;
+        float targetRatio = referenceWidth / referenceHeight;
+        if(screenRatio<targetRatio)
+        {
+            //屏幕比例小于目标比例，以宽度为基准
+            canvasScaler.matchWidthOrHeight = 0;
+        }
+        else
+        {
+            //屏幕比例大于目标比例，以高度为基准
+            canvasScaler.matchWidthOrHeight = 1;
+        }
+        referenceWidth = canvasScaler.referenceResolution.x;
+        referenceHeight = canvasScaler.referenceResolution.y;
+        
         targetRoot.anchorMin = new Vector2(0, minY);
         targetRoot.anchorMax = new Vector2(1, maxY);
         var baseRatio =referenceHeight/ referenceWidth;
